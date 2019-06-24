0b78c39c420f21484740b41261837b0e62dc9c68
jwillard <jwillard@clearent.com>
2019-06-24 17:49:28 -0500

    my first java class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jwillard on 6/24/2019.
+ */
+public class Main {
+
+
+    public static void main (String [] args){
+
+
+               System.out.println("hello world ");
+    }
+}
